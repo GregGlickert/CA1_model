23f8ae050ae45673e8521d74ca74133974ca3b9a
gregglickert <gglick9@gmail.com>
2021-08-09 10:52:48 -0500

    Update run_network.py

--- run_network.py
+++ run_network.py
@@ -1,8 +1,25 @@
+import os, sys
 from bmtk.simulator import bionet
+import numpy as np
+import synapses
+import warnings
 
-config_file = 'CA1_config.json'
-conf = bionet.Config.from_json(config_file, validate=True)
-conf.build_env()
-net = bionet.BioNetwork.from_config(conf)
-sim = bionet.BioSimulator.from_config(conf, network=net)
-sim.run()
+
+def run(config_file):
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    synapses.load()
+
+    conf = bionet.Config.from_json(config_file, validate=True)
+    conf.build_env()
+    graph = bionet.BioNetwork.from_config(conf)
+    sim = bionet.BioSimulator.from_config(conf, network=graph)
+
+    sim.run()
+    bionet.nrn.quit_execution()
+
+
+if __name__ == '__main__':
+    if __file__ != sys.argv[-1]:
+        run(sys.argv[-1])
+    else:
+        run('CA1_config.json')

4043176ffd2d7c40ce38fcc599c5a205e618d36a
gregglickert <gglick9@gmail.com>
2021-09-28 10:57:34 -0500

    added OLM cells

--- build_network.py
+++ build_network.py
@@ -17,12 +17,12 @@ np.random.seed(seed)
 print("placing cells in space")
 net = NetworkBuilder("biophysical")
 # amount of cells
-numAAC = 10  # 147
+numAAC = 147  # 147
 numCCK = 10  # 360
 numNGF = 10  # 580
-numOLM = 10  # 164
-numPV = 10  # 553
-numPyr = 10  # 31150   1000
+numOLM = 164  # 164
+numPV = 553  # 553
+numPyr = 31150  # 31150   1000
 # arrays for cell location csv
 cell_name = []
 cell_x = []
@@ -125,18 +125,18 @@ inds = np.random.choice(np.arange(0, np.size(pos_list_SO, 0)), numOLM, replace=F
 pos = pos_list_SO[inds, :]
 
 # place cell
-#net.add_nodes(N=numOLM, pop_name='OLM',
-#              positions=positions_list(positions=pos),
-#              mem_potential='e',
-#              model_type='biophysical',
-#              model_template='hoc:olmcell',
-#              morphology=None)
+net.add_nodes(N=numOLM, pop_name='OLM',
+              positions=positions_list(positions=pos),
+              mem_potential='e',
+              model_type='biophysical',
+              model_template='hoc:olmcell',
+              morphology=None)
 # save location in array delete used locations
-#for i in range(numOLM):
-#    cell_name.append("OLM in SO layer")
-#    cell_x.append(pos[i][0])
-#    cell_y.append(pos[i][1])
-#    cell_z.append(pos[i][2])
+for i in range(numOLM):
+    cell_name.append("OLM in SO layer")
+    cell_x.append(pos[i][0])
+    cell_y.append(pos[i][1])
+    cell_z.append(pos[i][2])
 
 pos_list_SO = np.delete(pos_list_SO, inds, 0)
 
@@ -523,32 +523,71 @@ conn = net.add_edges(source={'pop_name': 'PV'}, target={'pop_name': 'PV'},
                      target_sections=['somatic'],
                      sec_id=0,
                      sec_x=0.5)
-"""
+
 print('olm connections')
 # converge 8
 conn = net.add_edges(source={'pop_name': 'OLM'}, target={'pop_name': 'Pyr'},
                      connection_rule=n_connections,
-                     connection_params={'prob': 0.07015, 'max_dist': 500},
+                     connection_params={'prob': 0.07615, 'max_dist': 400},
                      syn_weight=1,
                      delay=0.1,
-                     dynamics_params='GABA_InhToExc.json',
-                     model_template='exp2syn',
-                     distance_range=[0.0, 500.0],
+                     dynamics_params='OLM2PN.json',
+                     model_template=syn['OLM2PN.json']['level_of_detail'],
+                     distance_range=[0.0, 400.0],
                      target_sections=['apical'],
-                     sec_id=0,
+                     sec_id=2,
                      sec_x=0.5)
+#convergence 8
+conn = net.add_edges(source={'pop_name': 'OLM'}, target={'pop_name': 'CHN'},
+                     connection_rule=n_connections,
+                     connection_params={'prob': 0.07615, 'max_dist': 400},
+                     syn_weight=1,
+                     delay=0.1,
+                     dynamics_params='OLM2CHN.json',
+                     model_template=syn['OLM2CHN.json']['level_of_detail'],
+                     distance_range=[0.0, 400.0],
+                     target_sections=['apical'],
+                     sec_id=2,
+                     sec_x=0.5)
+#convergence 8
+conn = net.add_edges(source={'pop_name': 'OLM'}, target={'pop_name': 'PV'},
+                     connection_rule=n_connections,
+                     connection_params={'prob': 0.07615, 'max_dist': 400},
+                     syn_weight=1,
+                     delay=0.1,
+                     dynamics_params='OLM2PV.json',
+                     model_template=syn['OLM2PV.json']['level_of_detail'],
+                     distance_range=[0.0, 400.0],
+                     target_sections=['apical'],
+                     sec_id=2,
+                     sec_x=0.5)
+#convergence of 6
+conn = net.add_edges(source={'pop_name': 'OLM'}, target={'pop_name': 'OLM'},
+                     connection_rule=n_connections,
+                     connection_params={'prob': 0.07615, 'max_dist': 400},
+                     syn_weight=1,
+                     delay=0.1,
+                     dynamics_params='OLM2OLM.json',
+                     model_template=syn['OLM2OLM.json']['level_of_detail'],
+                     distance_range=[0.0, 400.0],
+                     target_sections=['basal'],
+                     sec_id=2,
+                     sec_x=0.5)
+
 # converge 2378
 conn = net.add_edges(source={'pop_name': 'Pyr'}, target={'pop_name': 'OLM'},
                      connection_rule=n_connections,
-                     connection_params={'prob': 0.1095, 'max_dist': 500},
+                     connection_params={'prob': 0.1295, 'max_dist': 400},
                      syn_weight=1,
                      delay=0.1,
-                     dynamics_params='AMPA_ExcToInh.json',
-                     model_template='exp2syn',
-                     distance_range=[0.0, 500.0],
+                     dynamics_params='PN2OLM.json',
+                     model_template=syn['PVN2OLM.json']['level_of_detail'],
+                     distance_range=[0.0, 400.0],
                      target_sections=['basal'],
                      sec_id=0,
                      sec_x=0.5)
+
+"""
 #convergence of 13
 print("CCK connections")
 conn = net.add_edges(source={'pop_name': 'CCK'}, target={'pop_name': 'Pyr'},

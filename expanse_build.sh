69d870a2262fcb905b4f54cf702af1074feca21f
gregglickert <gglick9@gmail.com>
2022-01-05 15:58:17 -0600

    Update expanse_build.sh

--- expanse_build.sh
+++ expanse_build.sh
@@ -9,18 +9,12 @@
 #SBATCH --output=build.out
 #SBATCH --time 0-02:00
 
-
 module purge
 module load slurm
 module load cpu
 module load intel
 module load intel-mpi
 module load ncurses
-export PYTHONPATH=$HOME/nrn/lib/python:$PYTHONPATH
-export LD_LIBRARY_PATH=$HOME/nrn/x86_64/lib:$LD_LIBRARY_PATH
-export PATH=$HOME/nrn/x86_64/bin:$PATH
-
-rm -rf network/*
 
 echo "Building model at $(date)"
 

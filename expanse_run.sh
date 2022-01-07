09faf0475897491edc8c7179437e7bf1688ae6db
GregGlickert <59316745+GregGlickert@users.noreply.github.com>
2022-01-07 11:24:44 -0600

    Update expanse_run.sh

--- expanse_run.sh
+++ expanse_run.sh
@@ -2,28 +2,24 @@
 
 #SBATCH --partition shared
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=128
+#SBATCH --ntasks-per-node=120
 #SBATCH --account=umc113
 #SBATCH --job-name=run
 #SBATCH --output=run.out
-#SBATCH --mem=8G
+#SBATCH --mem=240G
 #SBATCH --time 0-48:00
 
 module purge
 module load slurm
 module load cpu
-module load intel
-module load intel-mpi
+module load gcc
+module load openmpi
 module load ncurses
-
 
 rm -rf output
 
-
 echo "Running model at $(date)"
 
-#mpirun nrniv -mpi -quiet -python3 run_network.py simulation_config.json
-ibrun nrniv -mpi -python run_network.py simulation_config.json
-#python run_network.py
+mpirun nrniv -mpi -python run_network.py simulation_config.json
 
 echo "Done running model at $(date)"
